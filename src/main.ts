46f9e8acb092fd48f268e9e090f561dfd605177a
younusraza909 <younus@geeksofkolachi.com>
2025-04-16 12:05:19 +0500

    converting to an instance of DTO

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, //extra property wont come if not there in DTO , this alone wont throw error
       forbidNonWhitelisted: true, //this will throw error if extra property comes, it works with whilelist config
+      transform: true, //After validation it will transform that property to the instance we have defined in DTO
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
