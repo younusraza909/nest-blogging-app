a2675ef5bc8a4ef07e02bb5941d24879c6031020
younusraza909 <younus@geeksofkolachi.com>
2025-04-20 14:25:51 +0500

    loading dynamic env using config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     UsersModule,
@@ -15,6 +17,8 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      // envFilePath: ['.env'],
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`, // Load different .env files based on the environment
     }),
     TypeOrmModule.forRootAsync({
       imports: [],
