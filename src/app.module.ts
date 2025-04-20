5eb7d339402ecdd878e6c634d8b058c6b397a334
younusraza909 <younus@geeksofkolachi.com>
2025-04-20 14:56:32 +0500

    registering config module as separate name space

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { appConfig } from './config/app.config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
 
 const ENV = process.env.NODE_ENV;
 @Module({
@@ -20,15 +21,15 @@ const ENV = process.env.NODE_ENV;
       isGlobal: true,
       // envFilePath: ['.env'],
       envFilePath: !ENV ? '.env' : `.env.${ENV}`, // Load different .env files based on the environment
-      load: [appConfig],
+      load: [appConfig, databaseConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        synchronize: true,
-        autoLoadEntities: true,
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
         port: +configService.get('database.port'), // this is the key from our app custom cofig
         host: configService.get('database.host'),
         username: configService.get('database.user'),
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,15 +1,5 @@
-import { env } from 'process';
+import { registerAs } from '@nestjs/config';
 
-export const appConfig = () => ({
-  environment: env.NODE_ENV || 'development',
-  database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT ?? '5342', 10) || 5432,
-    user: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    name: process.env.DATABASE_NAME || 'postgres',
-    syncronize: process.env.DATABASE_SYNCRONIZE === 'true' ? true : false,
-    autoLoadEntities:
-      process.env.DATABASE_AUTOLOAD_ENTITIES === 'true' ? true : false,
-  },
-});
+export default registerAs('app', () => ({
+  environment: process.env.NODE_ENV || 'development',
+}));
