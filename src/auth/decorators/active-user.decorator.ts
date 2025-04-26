a9384a68a30de16264f25f5fa621aefd7b0080ea
younusraza909 <younus@geeksofkolachi.com>
2025-04-26 16:22:14 +0500

    Add ActiveUser decorator for retrieving user information from the request

---
+++ src/auth/decorators/active-user.decorator.ts
@@ -0,0 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+interface ActiveUser {
+  sub: number;
+  email: string;
+}
+
+export const ActiveUser = createParamDecorator(
+  (field: keyof ActiveUser | undefined, ctx: ExecutionContext) => {
+    const request: Request = ctx.switchToHttp().getRequest();
+    const user = request['user'] as ActiveUser;
+    return field ? user[field] : user;
+  },
+);
